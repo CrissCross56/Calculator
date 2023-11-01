5c6ffa9758059e62ba32c2e934b8da7bf5bfc87f
CrissCross56 <grant.terdoslavich@gmail.com>
2023-11-01 04:01:59 -0400

    done with styling, time to host

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,121 @@
 //make a class for a calculator
 class Calculator{
-    constructor(previousOp,currentOp){
-        this.previousOp = previousOp
-        this.currentOp = currentOp
+    constructor(prevTextElement,currentTextElement){
+        this.prevTextElement = prevTextElement
+        this.currentTextElement = currentTextElement
+        this.clear()
     }
 
     clear(){
         this.previousOp = ''
         this.currentOp = ''
+        this.operation = undefined
+        this.updateDisplay()
     }
 
-    appendNumber(){
-
+    appendNumber(number){
+     
+            if(number === '.' && this.currentOp.includes('.')){
+                return
+            }
+            this.currentOp = this.currentOp.toString() + number.toString()
+        
+       
     }
 
-    //onec quereied for a result call this function
+    chooseOperation(operation){
+        if(this.currentOp === ''){
+            return
+        }
+        if(this.previousOp !== ""){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOp = this.currentOp
+        this.currentOp = ''
+    }
 
+    //make a delete function later, will need a div for the delete button
 
-    doMath(){
+     
+    compute(){
+        let computation
+        const prev = parseFloat(this.previousOp)
+        const curr = parseFloat(this.currentOp)
+        if(isNaN(prev) || isNaN(curr)){
+            return
+        }
+        switch(this.operation){
+            case '+':
+                computation = prev + curr
+                break
+             case '-':
+                computation = prev - curr
+                break
+            case '*':
+                computation = prev * curr
+                break
+            case '/':
+                computation = prev / curr
+                break
+            case '%':
+                computation = prev % curr
+                break
+            default:
+                return
+        }
+        this.currentOp = computation
+        this.operation = undefined
+        this.previousOp = ''
+    }
 
+    updateDisplay(){
+        this.currentTextElement.innerText = this.currentOp
+        this.prevTextElement.innerText = this.previousOp
     }
 }
 
 
 //setup dom elements for JS
-let input = document.querySelector("#input")
-//setup two different numbers
+const numberBtns = document.querySelectorAll('[data-number]')
+const opBtns = document.querySelectorAll('[data-operation]')
+const equalsBtn = document.querySelector('[data-equals]')
+const currentTextElement = document.querySelector('[data-current-operand]')
+const prevTextElement = document.querySelector('[data-previous-operand]')
+const dataClear = document.querySelector('[data-clear]')
+const dataEquals = document.querySelector('[data-equals]')
+const calculator = new Calculator(prevTextElement,currentTextElement)
 
+//add btns for the numbers
+numberBtns.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        calculator.appendNumber(btn.id);
+        //test to make sure clicking works
+        console.log("you are clicking " +btn.id)
+        calculator.updateDisplay()
+    })
+})
 
+//add btns for the operators
 
-let first = true;
+opBtns.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        calculator.chooseOperation(btn.id)
+        calculator.updateDisplay();
+    })
+})
+
+//make equals work
+equalsBtn.addEventListener('click',()=>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+dataClear.addEventListener('click', ()=>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
 const allowedKeys = ["1","2","3","4","5","6","7","8","9","0"]
 const operators =["+","-","=","*","/","%"]
-//when a key is pressed on the keyboard call a function to sort out what was pressed
-input.addEventListener('keydown',sort)
 
-
-//fucnctions go here
-
-function sort(e){
-
- 
-
-    //possibility taht an operator has been pressed before any values for numbers have been chosen
-    if(first && e.key){
-
-    }
-    //start holding user inout until they press an operator
-
-    //once an operator has been pressed we need to remove it from the firstNum
-
-    //
-}
-
-//add
-function math(first,second,operation){
-    //here we want to acquire one number, the operation to perform and then the second number
-    //the math should only execute once we provided a second number
-    
-
-}
-
-//subtract
+console.log(calculator.currentOp);
